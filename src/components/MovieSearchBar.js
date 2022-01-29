d99487e4324929d6c0975818b2ea977d3e65d189
blackenjoy27 <lixunxin@gmail.com>
2022-01-28 23:15:16 -0500

    setup the data binding between input and the state

--- src/components/MovieSearchBar.js
+++ src/components/MovieSearchBar.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
 
 const MovieSearchBar = (props) => {
+    const [keyword, setKeyword] = useState("");
+    const handleChange = (e) => {
+        setKeyword(e.target.value);
+        console.log(keyword);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(keyword);
+    }
     return (
-        <div>Hello World</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="keyword"
+                    value={keyword}
+                    onChange={handleChange}
+                    placeholder="I'm Looking for..."
+                />
+                <button type="submit">Search</button>
+            </form>
+        </div>
     )
 }
 
